c433644838484e575fa3069a308b0a8b9b046339
arcturusInk <baruaswati1995@gmail.com>
2024-03-07 01:24:53 -0500

    changed some values

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
 let particles = [];
-let mult = 0.005;
+let mult;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background('black');
   
-  let noiseVal = random(-10,6);
+  let noiseVal = random(-10,10);
   noiseDetail(noiseVal);
+
+  mult = random(0.003,0.0051);
+  //console.log(mult);
   
-  let density = 30;
+  let density = random(30,35);
   let space = width/density;
   
   for(let x = 0; x < width; x += space){
@@ -22,9 +25,9 @@ function setup() {
 function draw() {
   noStroke();
   fill(255);
-  
+
   for(let i = 0; i <particles.length; ++i){   
-   let angle = map(noise(particles[i].x * mult,particles[i].y * mult), 0, 1, 0, 720);
+   let angle = map(noise(particles[i].x * mult,particles[i].y * mult), 0, 2, 0, 720);
     particles[i].add(createVector(cos(angle),sin(angle)))
     ellipse(particles[i].x,particles[i].y, 1);
   }
